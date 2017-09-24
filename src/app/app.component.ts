daa16ade2a2350903fff03b062b2eadee2a8dc0c
Alex <AlexMalinki@gmail.com>
2017-09-24 18:07:28 +0200

    Add the background of the license

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   public types: Array<Option> = [
-    {title: 'test', value: 'url(../assets/images/type-bare.png)'},
-    {title: 'test', value: 'url(../assets/images/type-regular.png)'},
-    {title: 'test', value: 'url(../assets/images/type-short.png)'},
-    {title: 'test', value: 'url(../assets/images/type-winter.png)'},
+    {title: 'test', value: '../assets/images/type-bare.png'},
+    {title: 'test', value: '../assets/images/type-regular.png'},
+    {title: 'test', value: '../assets/images/type-short.png'},
+    {title: 'test', value: '../assets/images/type-winter.png'},
   ];
 
   public colors: Array<Option> = [
-    {title: 'test', value: 'red'},
-    {title: 'test', value: 'yellow'},
-    {title: 'test', value: 'blue'},
+    {title: 'test', value: 'black'},
+    {title: 'test', value: 'grey'},
+    {title: 'test', value: 'white'},
     {title: 'test', value: 'green'},
-    {title: 'test', value: 'purple'},
+    {title: 'test', value: 'lime'},
+    {title: 'test', value: 'blue'},
+    {title: 'test', value: 'red'},
     {title: 'test', value: 'orange'},
+    {title: 'test', value: 'yellow'},
+    {title: 'test', value: 'purple'},
     {title: 'test', value: 'pink'},
   ];
 
   public patterns: Array<Option> = [
-    {title: 'test', value: 'url(../assets/images/pattern-argyle.png)'},
-    {title: 'test', value: 'url(../assets/images/pattern-dots.png)'},
-    {title: 'test', value: 'url(../assets/images/pattern-solid.png)'},
-    {title: 'test', value: 'url(../assets/images/pattern-stripes-diagonally.png)'},
-    {title: 'test', value: 'url(../assets/images/pattern-stripes-horizontally.png)'},
-    {title: 'test', value: 'url(../assets/images/pattern-stripes-vertically.png)'},
+    {title: 'test', value: '../assets/images/pattern-solid.png'},
+    {title: 'test', value: '../assets/images/pattern-argyle.png'},
+    {title: 'test', value: '../assets/images/pattern-dots.png'},
+    {title: 'test', value: '../assets/images/pattern-stripes-diagonally.png'},
+    {title: 'test', value: '../assets/images/pattern-stripes-horizontally.png'},
+    {title: 'test', value: '../assets/images/pattern-stripes-vertically.png'},
   ];
 
   public printBtnClick() {
